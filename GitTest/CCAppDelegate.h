df9e7fe6c9d4ae342340b3b55f906704b63526ec
Pradeep Kumar <pradeep@Pradeeps-MacBook-Air.local>
2014-12-16 16:59:26 -0600

    first commit

---
+++ GitTest/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  GitTest
+//
+//  Created by Pradeep Kumar on 12/16/14.
+//  Copyright (c) 2014 Pradeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  GitTest
+//
+//  Created by Pradeep Kumar on 12/16/14.
+//  Copyright (c) 2014 Pradeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
