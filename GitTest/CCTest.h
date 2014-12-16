05c4cf3d73fdb4814b186e01a41f3a5cdefaa73f
SterlingArcherr <sterling.archer@ymail.com>
2014-12-16 17:05:33 -0600

    added a test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Pradeep Kumar on 12/16/14.
+//  Copyright (c) 2014 Pradeep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Pradeep Kumar on 12/16/14.
+//  Copyright (c) 2014 Pradeep. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
